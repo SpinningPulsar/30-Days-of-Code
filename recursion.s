5374e3cecf14afc127a01e2081b0ceee9b75740d
Srivatsen <mayurnath.me@gmail.com>
2024-04-20 10:21:26 +0530

    new file:   recursion.s

---
+++ recursion.s
@@ -0,0 +1,63 @@
+	.file	"recursion.c"
+	.text
+	.globl	recursion
+	.def	recursion;	.scl	2;	.type	32;	.endef
+	.seh_proc	recursion
+recursion:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$32, %rsp
+	.seh_stackalloc	32
+	.seh_endprologue
+	movl	%ecx, 16(%rbp)
+	cmpl	$1, 16(%rbp)
+	jle	.L2
+	movl	16(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, %ecx
+	call	recursion
+	imull	16(%rbp), %eax
+	jmp	.L3
+.L2:
+	movl	$1, %eax
+.L3:
+	addq	$32, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.def	__main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+.LC0:
+	.ascii "%d\0"
+	.text
+	.globl	main
+	.def	main;	.scl	2;	.type	32;	.endef
+	.seh_proc	main
+main:
+	pushq	%rbp
+	.seh_pushreg	%rbp
+	movq	%rsp, %rbp
+	.seh_setframe	%rbp, 0
+	subq	$48, %rsp
+	.seh_stackalloc	48
+	.seh_endprologue
+	call	__main
+	movl	$10, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %ecx
+	call	recursion
+	movl	%eax, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %edx
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rcx
+	call	printf
+	movl	$0, %eax
+	addq	$48, %rsp
+	popq	%rbp
+	ret
+	.seh_endproc
+	.ident	"GCC: (Rev3, Built by MSYS2 project) 13.2.0"
+	.def	printf;	.scl	2;	.type	32;	.endef
